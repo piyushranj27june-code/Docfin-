788438f30a753e15f1999805c1d050c527ae1166
piyushranj27june-code <piyushranj27june@gmail.com>
2026-06-23 18:46:21 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -45,6 +45,10 @@ class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
+class ChangePassword(BaseModel):
+    current_password: str
+    new_password: str
+
 class UserOut(BaseModel):
     id: str
     email: str
